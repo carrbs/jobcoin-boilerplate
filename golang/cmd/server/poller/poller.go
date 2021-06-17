fe2b2cc7dab892e76fd09ea30d45dc93ff8ae516
Ben Carr <carrbens@gmail.com>
2021-06-17 16:06:36 -0700

    make poller use Address structs instead of strings (for clarity); also create a AddressResponse to pass the balance around more easily

--- golang/cmd/server/poller/poller.go
+++ golang/cmd/server/poller/poller.go
@@ -4,43 +4,36 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
-	"strconv"
 	"time"
 
-	"github.com/gemini/jobcoin"
+	"github.com/gemini/jobcoin/cmd/server/mixer"
 	"github.com/gemini/jobcoin/db"
+	"github.com/gemini/jobcoin/models/addresses"
 )
 
 type DepositAddressPoller struct {
 	ticker    *time.Ticker
-	add       chan DepositAddress
-	addresses []DepositAddress
-}
-
-type DepositAddress struct {
-	Address string
-}
-
-func (a *DepositAddress) URL() string {
-	return jobcoin.AddressesEndpoint + "/" + a.Address
+	add       chan addresses.DepositAddress
+	addresses []addresses.DepositAddress
 }
 
 func NewDepositPoller() *DepositAddressPoller {
 	rv := &DepositAddressPoller{
-		ticker: time.NewTicker(time.Second * 1),
-		add:    make(chan DepositAddress),
+		ticker: time.NewTicker(time.Second * 10),
+		add:    make(chan addresses.DepositAddress),
 	}
 	go rv.run()
 	return rv
 }
 
-func InitializeDepositPoller() error {
+func InitializeDepositPoller() (*DepositAddressPoller, error) {
 	poller := NewDepositPoller()
 	if err := registerDepositAddresses(poller); err != nil {
-		return err
+		return poller, err
 	}
-	return nil
+	return poller, nil
 }
 
 func registerDepositAddresses(h *DepositAddressPoller) error {
@@ -61,8 +54,7 @@ func (h *DepositAddressPoller) run() {
 		select {
 		case <-h.ticker.C:
 			for _, address := range h.addresses {
-				url := address.URL()
-				processDepositAddresses(url, client)
+				processDepositAddresses(&address, client)
 			}
 		case address := <-h.add:
 			h.addresses = append(h.addresses, address)
@@ -70,74 +62,51 @@ func (h *DepositAddressPoller) run() {
 	}
 }
 
-func (h *DepositAddressPoller) AddURL(address *DepositAddress) {
+func (h *DepositAddressPoller) AddURL(address *addresses.DepositAddress) {
 	fmt.Println("new deposit address: ", address)
 	h.add <- *address
 }
-func GetDepositAddresses() ([]*DepositAddress, error) {
-	var addresses []*DepositAddress
+func GetDepositAddresses() ([]*addresses.DepositAddress, error) {
+	var addrs []*addresses.DepositAddress
 	conn := db.DBConn()
 	defer conn.Close()
 	rows, err := conn.Query("SELECT DISTINCT(deposit_address) from deposit_addresses")
 	if err != nil {
-		return addresses, err
+		return addrs, err
 	}
 	var address string
 	for rows.Next() {
 		rows.Scan(&address)
-		addresses = append(addresses, NewDepositAddress(address))
+		addrs = append(addrs, addresses.NewDepositAddress(address))
 	}
-	return addresses, nil
+	return addrs, nil
 }
 
-func NewDepositAddress(address string) *DepositAddress {
-	return &DepositAddress{Address: address}
-}
-
-func processDepositAddresses(url string, client *http.Client) error {
-	req, err := http.NewRequest("GET", url, nil)
+func processDepositAddresses(address *addresses.DepositAddress, client *http.Client) error {
+	req, err := http.NewRequest("GET", address.URL(), nil)
 	if err != nil {
-		fmt.Print(err.Error())
+		return err
 	}
 	req.Header.Add("Accept", "application/json")
 	req.Header.Add("Content-Type", "application/json")
 	resp, err := client.Do(req)
 	if err != nil {
-		fmt.Print(err.Error())
+		return err
 	}
 	defer resp.Body.Close()
 	bodyBytes, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		fmt.Print(err.Error())
+		return err
 	}
-	var addressInfo AddressResponse
+	var addressInfo addresses.AddressResponse
 	json.Unmarshal(bodyBytes, &addressInfo)
-	fmt.Println("bodyBytes", string(bodyBytes))
-
-	fmt.Printf("API Response as struct %+v\n", addressInfo)
-
-	if addressInfo.balance() > 0 {
-		// Mix()
-		fmt.Println("ALERT! Positive BALANCE!", addressInfo.Balance)
+	log.Println("heartbeat ( address:", address.Address, ")")
+	if addressInfo.BalanceFloat() > 0 {
+		fmt.Println("Mixing ", address.Address, "(amount): ", addressInfo.Balance)
+		if err := mixer.Mix(address, addressInfo); err != nil {
+			return err
+		}
 	}
 
 	return nil
-}
-
-type Transaction struct {
-	Timestamp   time.Time `json:"timestamp"`
-	ToAddress   string    `json:"toAddress"`
-	FromAddress string    `json:"fromAddress"`
-	Amount      string    `json:"amount"`
-}
-
-type AddressResponse struct {
-	Balance      string        `json:"balance"`
-	Transactions []Transaction `json:"transactions"`
-}
-
-func (a *AddressResponse) balance() float64 {
-	// FIXME: handle error
-	f, _ := strconv.ParseFloat(a.Balance, 64)
-	return f
 }
